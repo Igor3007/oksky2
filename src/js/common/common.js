cf8063fb2a9a7344a7eb05fa770675054b328dd7
Igor <50610898+Igor3007@users.noreply.github.com>
2024-02-05 12:39:56 +0300

    add header

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -4,8 +4,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
         if (document.documentElement.scrollTop > 200) {
             document.querySelector('.header').classList.add('header--white')
         } else {
-            document.querySelector('.header').classList.contains('header--white') || document.querySelector('.header').classList.remove('header--white')
+            !document.querySelector('.header').classList.contains('header--white') || document.querySelector('.header').classList.remove('header--white')
         }
     })
+
+    /* ============================
+    menu
+    ============================*/
+
+
+    if (document.querySelector('[data-menu="open"]')) {
+        const btnBurger = document.querySelector('[data-menu="open"]')
+        const menuBurger = document.querySelector('[data-menu="container"]')
+
+        btnBurger.addEventListener('click', e => {
+            btnBurger.classList.toggle('open')
+            menuBurger.classList.toggle('open')
+            document.body.classList.toggle('page-hidden')
+        })
+
+        menuBurger.querySelectorAll('.isset-sub').forEach(item => {
+            item.addEventListener('click', e => {
+                e.stopPropagation()
+
+                if (e.target.classList.contains('open')) {
+                    e.target.classList.remove('open')
+                    return false;
+                }
+
+                e.target.closest('ul').querySelectorAll('.open').forEach(li => {
+                    li.classList.remove('open')
+                })
+
+                e.target.classList.toggle('open')
+            })
+        })
+    }
+
+
 
 })
